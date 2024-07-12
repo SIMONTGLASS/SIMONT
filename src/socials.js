fab0ddf372207e90c1ece2510ee1b132f3ccc213
Yanguangchen <yanguangchen@outlook.com>
2024-07-12 19:09:52 +0800

    commits

--- src/socials.js
+++ src/socials.js
@@ -1,93 +1,32 @@
-import React, { useState } from "react";
-import storeImge from "./Assets/GoogleBusinessProfileImages.webp";
 import { ElfsightWidget } from "react-elfsight-widget";
 import GoogleReviews from "./reviewCarousell";
 
 function Socials() {
-  // Step 1: Initialize state
-  const [showimage, setShowimage] = useState(false);
-
-  console.log("useState");
-  console.log(showimage);
-
   const flexContainer = {
     marginTop: "10%",
     display: "flex",
     flexDirection: "column",
-    justifyContent: "space-between",
-    alignItems: "center",
-  };
-
-  const flexContainer1 = {
-    display: "flex",
-    flexDirection: "column",
-    justifyContent: "space-between",
+    justifyContent: "center",
     alignItems: "center",
     width: "100%",
   };
-
-  const buttonStyle = {
-    marginTop: "5%",
-    fontSize: "100%",
-    padding: "1%",
-    fontFamily: "Montserrat, sans-serif",
-    backgroundColor: "#051d40",
-    color: "white",
-  };
-
-  const photoStyle = {
-    marginTop: "5%",
-    display: showimage ? "initial" : "none",
-    width: "100%",
-  };
-
-  // const fbLogoStyle = {
-  //   marginTop: "3%",
-  //   width: "10%",
-  //   height: "10%",
-  // };
-
 
   const widget1 = {
-    width: "80%",
+    width: "100%",
+    display: "flex",
+    flexDirection: "column",
+    justifyContent: "center",
+    alignItems: "center",
   };
 
   return (
     <div style={flexContainer}>
-      <div style={widget1}>
+      <div>
         <GoogleReviews />
-      </div>{" "}
-      
-      {/* <ElfsightWidget
-        style={widget1}
-        widgetId="977f6169-8a58-40e9-8ba0-6b4e50d534d9"
-      /> */}
-      <div style={flexContainer}>
-        {/* <a
-          className="socials-container"
-          style={aStyle}
-          href="https://www.facebook.com"
-        >
-          Our facebook page
-          <img
-            src={fbLogo}
-            style={fbLogoStyle}
-            className="storeButton"
-            alt="fblogo"
-            loading="lazy"
-          />
-        </a> */}
+      </div>
 
-        {/* <button
-          style={btn}
-          className="storeButton"
-          onClick={() => {
-            window.location.href =
-              "https://www.facebook.com/profile.php?id=61561731779552";
-          }}
-        >
-          Click this here to visit
-        </button> */}
+      <div style={{border: "0.5px solid lightgray", marginTop: "10%"}}>
+        <ElfsightWidget widgetId="85f882e3-2660-4675-b069-0ab29ba97f91" />
       </div>
     </div>
   );
